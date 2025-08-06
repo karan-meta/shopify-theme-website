00950143d9a5e7b916b4e5c67f92d7781834bb0c
karan-meta <karan.kaushik@metadologie.com>
2025-08-06 21:23:16 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,92 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import bgImg from "./assets/pics/homepage_hero_shopify.webp";
+
+function App() {
+  return (
+    <>
+      <div>
+        <div className="relative w-full h-screen">
+          <img
+            src="./src/assets/pics/homepage_hero_shopify.webp"
+            alt="background"
+            className="absolute inset-0 w-full h-full object-cover -z-10"
+          ></img>
+          <div className="flex justify-between items-center p-4 w-full ">
+            <div className="flex space-x-4">
+              <h1 className="text-white">Theme Features</h1>
+              <a className="text-white">Blog</a>
+              <a className="text-white">FAQ</a>
+            </div>
+            <div className="flex space-x-4">
+              <i
+                className="fab fa-instagram text-white text-1xl"
+              
+              ></i>
+              <a className="text-white">Gift Card</a>
+              <a className="text-white">English</a>
+              <a className="text-white">Cad$</a>
+            </div>
+          </div>
+          <hr className="bg-white h-px border-0 "></hr>
+          <div className="flex justify-between items-center p-4 w-full ">
+            <div className="flex space-x-4">
+              <h1 className="text-white">Jwellery</h1>
+              <h1 className="text-white">Sale</h1>
+              <h1 className="text-white">About</h1>
+            </div>
+            <div className="flex space-x-4">
+              <h1 className="text-white text-3xl">Broadcast</h1>
+            </div>
+            <div className="flex space-x-4">
+              <i className="fa-regular fa-circle-user text-1xl text-white"></i>
+              <img src="./src/assets/pics/magnifying_glass.png " className="invert brightness-[200%] h-5"></img>
+              <img src="./src/assets/pics/shopping_bag.png" className="invert h-5"></img>
+            </div>
+          </div>
+          <div className="relative w-fit top-60 left-10 flex flex-col space-y-6">
+            <h1 className="text-white text-5xl w-fit font-bold">EVERYDAY LUXURY</h1>
+            <div className="space-y-2">
+              <h1 className="text-white text-2xl w-fit">Beautiful pieces to pass down to generations</h1>
+              <div className="space-x-5">
+                <button className="bg-black text-white h-15 w-45">  SHOP ALL PRODUCTS</button>
+                <button className="bg-white text-black h-15 w-30">SHOP SALE</button>
+              </div>
+            </div>
+          </div>
+
+        </div>
+        <div className="flex justify-center w-full pt-20 pb-20">
+          <p className="text-3xl font-bold">
+            SHOP OUR MODERN <img src="src\assets\pics\short_jewel_image.avif" className="inline-block h-20"></img> HEIRLOOMS , BESTSELLING <br></br>FINE JWELERY <img src="src\assets\pics\short_ring_image.avif" className="inline-block h-20"></img> AND ELEGANT <img src="src\assets\pics\short_necklace.avif" className="inline-block h-20"></img> TREASURES.
+          </p>
+       
+        </div>
+        <div className="flex justify-around">
+          <div className="w-80 space-y-4">
+            <img src="src\assets\pics\Broadcast_Jewelry.webp" className="h-100"></img>
+            <p className="font-bold">THE ESSENTIALS COLLECTION</p>
+            <p>Our signature pieces designed to be worn everyday with subtle details in contemporary shapes.</p>
+            <p>SHOP CLASSICS <i class="fa-solid fa-arrow-right"></i></p>
+          </div>
+          <div className="w-80 space-y-4">
+            <img src="src\assets\pics\modern_statement.webp" className="h-100"></img>
+             <p className="font-bold">MODERN STATEMENT</p>
+            <p>Timeless and wearable earrings with a modern twist that add a touch of style to any outfit.</p>
+            <p>SHOP EARINGS <i class="fa-solid fa-arrow-right"></i></p>
+          </div>
+          <div className="w-80 space-y-4">
+            <img src="src\assets\pics\made_to_order.webp" className="h-100"></img>
+             <p className="font-bold">MADE-TO-ORDER JEWELERY</p>
+            <p>Handmade pieces that are carefully crafted to ensure that you'll love for a lifetime.</p>
+            <p>SHOP NOW <i class="fa-solid fa-arrow-right"></i></p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
